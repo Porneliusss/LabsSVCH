68d7afdb991f37684e475e171376999c10767cb4
Porneliusss <11111pyk@mail.ru>
2024-01-11 12:51:50 +0300

    add lab8

---
+++ lab8/app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const fs = require("fs").promises;
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+const port = 3000;
+const dataFilePath = path.join(__dirname, "products.json");
+let productsData = require("./products.json");
+
+app.get("/api/products", (req, res) => {
+  res.json(productsData);
+});
+
+app.post("/api/products", (req, res) => {
+  res.json(productsData);
+});
+
+app.post("/api/products/add", (req, res) => { 
+  const newProduct = req.body;
+  newProduct.id = productsData.length + 1;
+  productsData.push(newProduct);
+
+  fs.writeFile(
+    "./products.json",
+    JSON.stringify(productsData, null, 2),
+    "utf8"
+  );
+
+  res.json({
+    message: "Product added successfully",
+    product: newProduct,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/formattedData", async (req, res) => {
+  try {
+    const rawData = await fs.readFile(dataFilePath, "utf8");
+    const data = JSON.parse(rawData);
+    const acceptHeader = req.get("Accept");
+
+    if (acceptHeader.includes("text/html")) {
+      const htmlResponse = data
+        .map(
+          (product) =>
+            `<div>ID: ${product.id}, Name: ${product.name}, Type: ${product.type}, Quality: ${product.quality}</div>`
+        )
+        .join("");
+      res.send(htmlResponse);
+    } else if (acceptHeader.includes("application/xml")) {
+      const xmlResponse = `<data>${data
+        .map(
+          (product) =>
+            `<product><id>${product.id}</id><name>${product.name}</name><type>${product.type}</type><quality>${product.quality}</quality></product>`
+        )
+        .join("")}</data>`;
+      res.type("application/xml");
+      res.send(xmlResponse);
+    } else {
+      res.json(rawData);
+    }
+  } catch (error) {
+    console.error("Error reading or parsing data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
