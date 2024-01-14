2ffcb036dbda60c46d1d71a4b16dc6c1f0a57b90
Porneliusss <11111pyk@mail.ru>
2024-01-14 14:14:58 +0300

    lab11

--- lab9/app/src/server/db.js
+++
@@ -1,7 +0,0 @@
-const { Sequelize } = require("sequelize");
-
-module.exports = new Sequelize("ProductQuality", "postgres", "1111", {
-  dialect: "postgres",
-  host: "localhost",
-  port: "5432",
-});
